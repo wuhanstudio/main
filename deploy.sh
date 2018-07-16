ba356050874da3c8c01c836e5d0624b7cf07d939
Wu Han <wuhanstudio@hust.edu.cn>
2018-07-16 10:35:03 +0800

    Fix deploy.sh with notfound.html & working.html

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -r src/mpw src/focus ./dist
-cp src/{*.png,*.svg,*.xml,*.ico,manifest.json} ./dist
+cp src/{*.png,*.svg,*.xml,*.ico,manifest.json,working.html,notfound.html} ./dist
 
 if [ -e wuhanstudio.tar.gz ] 
 then
