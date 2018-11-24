e7dfa7738f8e093105da17c6323a3541f5537250
Wu Han <wuhanstudio@hust.edu.cn>
2018-11-24 23:39:33 +0000

    Aded robots idea and struture

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -22,6 +22,21 @@ $(document).ready(function () {
         preloader: false,
         fixedContentPos: false
     });
+    
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: false
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 
     var gradTimer, sideTimer, comTimer;
     $('#fullpage').fullpage({
